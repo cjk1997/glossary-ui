4ff5b3b073d358503f3d845e1cff57603aa8a794
Carson Keller <carsonjkeller@gmail.com>
2020-05-05 16:48:09 -0600

    Reinstated index file and integrated main Glossary.js file to be rendered

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Glossary from './Glossary';
+
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Glossary />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
